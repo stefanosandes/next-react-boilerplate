7cc7fa5e5a28cf977eb484de50b47264811d0510
Stefano Sandes <stefanosandes@gmail.com>
2020-07-05 16:00:49 -0300

    workbook and tests

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,23 @@
-const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+import * as S from './styles'
+
+interface Props {
+  title?: string
+  description?: string
+}
+
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}: Props) => (
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Logomarca do site" />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para a uma tela com código"
+    />
+  </S.Wrapper>
 )
 
 export default Main
